8ef3a235baf28927aec43c554a47f4a7eee14969
Enzindc <enzo.barcelos.rios@gmail.com>
2023-10-15 18:03:19 -0300

    novo menu

--- codigo/MenuTerminal.java
+++ codigo/MenuTerminal.java
@@ -20,6 +20,7 @@ public class MenuTerminal {
             System.out.println("0. Sair");
             System.out.print("Opção: ");
             opcao = sc.nextInt();
+            sc.nextLine();  
 
             switch (opcao) {
                 case 1:
@@ -46,10 +47,129 @@ public class MenuTerminal {
         } while (opcao != 0);
     }
 
-    
+    private static void cadastrarCliente() {
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        System.out.print("CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Senha: ");
+        String senha = sc.nextLine();
 
-}
+        Cliente cliente = new Cliente(nome, cpf, senha);
+        clientes.add(cliente);
+        System.out.println("Cliente cadastrado com sucesso!");
+    }
 
-public static void main(String[] args) {
-    MenuTerminal.iniciar();
+    private static Cliente buscarClientePorCPF(String cpf) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getCpf().equals(cpf)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    private static void abrirConta() {
+        System.out.print("CPF do cliente: ");
+        String cpf = sc.nextLine();
+        Cliente cliente = buscarClientePorCPF(cpf);
+
+        if (cliente == null) {
+            System.out.println("Cliente não encontrado!");
+            return;
+        }
+
+        System.out.println("Tipo de Conta (1-Corrente, 2-Poupanca): ");
+        int tipo = sc.nextInt();
+        sc.nextLine();
+
+        Conta conta;
+
+        switch (tipo) {
+            case 1:
+                conta = new ContaCorrente();
+                break;
+            case 2:
+                conta = new ContaPoupanca();
+                break;
+            default:
+                System.out.println("Tipo inválido!");
+                return;
+        }
+
+        cliente.addConta(conta);
+        System.out.println("Conta criada com sucesso!");
+    }
+
+    private static void depositarConta() {
+        System.out.print("CPF do cliente: ");
+        String cpf = sc.nextLine();
+        Cliente cliente = buscarClientePorCPF(cpf);
+
+        if (cliente == null) {
+            System.out.println("Cliente não encontrado!");
+            return;
+        }
+
+        System.out.println("Informe o número da conta: ");
+        int numeroConta = sc.nextInt();
+        sc.nextLine();
+
+        Conta conta = cliente.getContaByNumber(numeroConta);
+        if (conta == null) {
+            System.out.println("Conta não encontrada!");
+            return;
+        }
+
+        System.out.print("Valor a depositar: ");
+        double valor = sc.nextDouble();
+        sc.nextLine();
+
+        conta.depositar(valor);
+        System.out.println("Depósito realizado com sucesso!");
+    }
+
+    private static void sacarConta() {
+        System.out.print("CPF do cliente: ");
+        String cpf = sc.nextLine();
+        Cliente cliente = buscarClientePorCPF(cpf);
+
+        if (cliente == null) {
+            System.out.println("Cliente não encontrado!");
+            return;
+        }
+
+        System.out.println("Informe o número da conta: ");
+        int numeroConta = sc.nextInt();
+        sc.nextLine();
+
+        Conta conta = cliente.getContaByNumber(numeroConta);
+        if (conta == null) {
+            System.out.println("Conta não encontrada!");
+            return;
+        }
+
+        System.out.print("Valor a sacar: ");
+        double valor = sc.nextDouble();
+        sc.nextLine();
+
+        conta.sacar(valor);
+        System.out.println("Saque realizado com sucesso!");
+    }
+
+    private static void visaoDiretoria() {
+        
+        System.out.println("=== Visão da Diretoria ===");
+        for (Cliente cliente : clientes) {
+            System.out.println("Cliente: " + cliente.getNome());
+            for (Conta conta : cliente.getContas()) {
+                System.out.println("Conta: " + conta.getNumero() + " - Saldo: " + conta.getSaldo());
+            }
+            System.out.println("-----------------------------");
+        }
+    }
+
+    public static void main(String[] args) {
+        MenuTerminal.iniciar();
+    }
 }
