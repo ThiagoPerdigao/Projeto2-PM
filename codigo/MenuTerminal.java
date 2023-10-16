f977c6ea6727dc7bab1aef1cdd547c5680e48f76
Enzindc <enzo.barcelos.rios@gmail.com>
2023-10-15 22:27:51 -0300

    menu definitivo

--- codigo/MenuTerminal.java
+++ codigo/MenuTerminal.java
@@ -1,39 +1,72 @@
 import java.util.Scanner;
 
 public class App {
-    private static Scanner scanner = new Scanner(System.in);
-    private static MenuTerminal menuTerminal = new MenuTerminal();
-
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String opcao;
 
         do {
             System.out.println("\n=== Menu do XuBank ===");
-            System.out.println("a. Listar todos os clientes");
-            System.out.println("b. Verificar o saldo de um cliente específico");
-            System.out.println("c. Listar todas as contas e seus saldos");
-            System.out.println("d. Ver os detalhes de uma conta específica");
+            System.out.println("a. Realizar operação");
+            System.out.println("b. Investir em Renda Fixa");
+            System.out.println("c. Ver visão da diretoria");
+            System.out.println("d. Fazer investimento");
+            System.out.println("e. Acessar conta poupança");
+            System.out.println("f. Acessar conta corrente");
+            System.out.println("g. Acessar conta");
+            System.out.println("h. Cliente VIP");
+            System.out.println("i. Cliente Regular");
+            System.out.println("j. Cliente Gold");
+            System.out.println("k. Acessar cliente");
             System.out.println("x. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextLine();
 
             switch (opcao) {
                 case "a":
-                    menuTerminal.listarClientes();
+                    Operacao operacao = new Operacao();
+                    operacao.executar();
                     break;
                 case "b":
-                    System.out.print("Informe o CPF do cliente: ");
-                    String cpfCliente = scanner.nextLine();
-                    menuTerminal.verificarSaldoCliente(cpfCliente);
+                    RendaFixa rendaFixa = new RendaFixa();
+                    rendaFixa.executar();
                     break;
                 case "c":
-                    menuTerminal.listarContas();
+                    VisaoDiretoria visaoDiretoria = new VisaoDiretoria();
+                    visaoDiretoria.executar();
                     break;
                 case "d":
-                    System.out.print("Informe o número da conta: ");
-                    int numeroConta = scanner.nextInt();
-                    scanner.nextLine(); 
-                    menuTerminal.verDetalhesConta(numeroConta);
+                    Investimento investimento = new Investimento();
+                    investimento.executar();
+                    break;
+                case "e":
+                    ContaPoupanca contaPoupanca = new ContaPoupanca();
+                    contaPoupanca.executar();
+                    break;
+                case "f":
+                    ContaCorrente contaCorrente = new ContaCorrente();
+                    contaCorrente.executar();
+                    break;
+                case "g":
+                    Conta conta = new Conta();
+                    conta.executar();
+                    break;
+                case "h":
+                    ClienteVIP clienteVIP = new ClienteVIP();
+                    clienteVIP.executar();
+                    break;
+                case "i":
+                    ClienteRegular clienteRegular = new ClienteRegular();
+                    clienteRegular.executar();
+                    break;
+                case "j":
+                    ClienteGold clienteGold = new ClienteGold();
+                    clienteGold.executar();
+                    break;
+                case "k":
+                    Cliente cliente = new Cliente();
+                    cliente.executar();
                     break;
                 case "x":
                     System.out.println("Saindo...");
@@ -45,41 +78,4 @@ public class App {
 
         scanner.close();
     }
-}
-
-class MenuTerminal {
-    private static List<Cliente> clientes = new ArrayList<>();
-
-    public void listarClientes() {
-        for (Cliente cliente : clientes) {
-            System.out.println(cliente.toString());
-        }
-    }
-
-    public void verificarSaldoCliente(String cpf) {
-        Cliente cliente = buscarClientePorCPF(cpf);
-        if (cliente != null) {
-            System.out.println("Saldo do cliente " + cliente.getNome() + ": " + cliente.getConta().getSaldo());
-        } else {
-            System.out.println("Cliente não encontrado!");
-        }
-    }
-
-    public void listarContas() {
-        for (Cliente cliente : clientes) {
-            Conta conta = cliente.getConta();
-            System.out.println("Conta número: " + conta.getNumero() + ", Saldo: " + conta.getSaldo());
-        }
-    }
-
-    public void verDetalhesConta(int numeroConta) {
-        Conta conta = buscarContaPorNumero(numeroConta);
-        if (conta != null) {
-            System.out.println(conta.toString());
-        } else {
-            System.out.println("Conta não encontrada!");
-        }
-    }
-
-    
 }
