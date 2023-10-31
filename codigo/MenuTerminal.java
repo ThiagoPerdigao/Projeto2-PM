76ab8134863b8186f543f3a3476311f1a9337a5b
Enzindc <enzo.barcelos.rios@gmail.com>
2023-10-31 11:07:55 -0300

    menu definitivo

--- codigo/MenuTerminal.java
+++ codigo/MenuTerminal.java
@@ -1,80 +1,120 @@
 import java.util.Scanner;
 
+
 public class MenuTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String opcao;
+        Investimento investimento = new Investimento();
+        ContaCorrente contaCorrente = new ContaCorrente();
+        ContaPoupanca poupanca = new ContaPoupanca();
+        RendaFixa rendaFixa = new RendaFixa();
+        
+        
 
         do {
-            System.out.println("\n=== Menu do XuBank ===");
-            System.out.println("a. Realizar operação");
-            System.out.println("b. Investir em Renda Fixa");
-            System.out.println("c. Ver visão da diretoria");
-            System.out.println("d. Fazer investimento");
-            System.out.println("e. Acessar conta poupança");
-            System.out.println("f. Acessar conta corrente");
-            System.out.println("g. Acessar conta");
-            System.out.println("h. Cliente VIP");
-            System.out.println("i. Cliente Regular");
-            System.out.println("j. Cliente Gold");
-            System.out.println("k. Acessar cliente");
-            System.out.println("x. Sair");
+            System.out.println("=== Menu do XuBank ===");
+            System.out.println("1. === Consulta de saldo ===");
+            System.out.println("2. === Depósito ===");
+            System.out.println("3. === Saque ===");
+            System.out.println("4. === Transferências ===");
+            System.out.println("5. === Adicionar cliente ===");
+            System.out.println("6. === Adicionar ClienteGOLD ===");
+            System.out.println("7. === Adicionar ClienteVIP ===");
+            System.out.println("8. === Calcular Rendimento Diario ===");
+            System.out.println("9. === Fazer saque especial da conta corrente ===");
+            System.out.println("10. === Verificar Rendimento Poupanca ===");
+            System.out.println("11. === Calcular Rendimento Renda Fixa ===");
+            System.out.println("12. === Realizar Saque Renda Fixa ===");
+            System.out.println("13. === Acesso Diretoria ===");
+            
+            
+            
+            
+            System.out.println("13. Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextLine();
 
             switch (opcao) {
-                case "a":
-                    Operacao operacao = new Operacao();
-                    operacao.executar();
+                case "1":
+                	System.out.println("Voce possui R$" + investimento.getSaldo() + " em sua conta");
+                	break;
+                case "2":
+                	investimento.deposito();
                     break;
-                case "b":
-                    RendaFixa rendaFixa = new RendaFixa();
-                    rendaFixa.executar();
+                case "3":
+                	investimento.saque();
                     break;
-                case "c":
-                    VisaoDiretoria visaoDiretoria = new VisaoDiretoria();
-                    visaoDiretoria.executar();
+                case "4":
+                	investimento.transferencia(investimento);
                     break;
-                case "d":
-                    Investimento investimento = new Investimento();
-                    investimento.executar();
+                case "5":
+                	System.out.println("---- SISTEMA DE CRIAÇÃO DE CONTA REGULAR ----");
+                	System.out.println("---- Qual nome do usuario ----");
+                	String nome = scanner.nextLine();
+                	System.out.println("---- Qual o CPF do usuario----");
+                	String cpf = scanner.nextLine();
+                	System.out.println("---- Qual a Senha do usuario----");
+                	String senha = scanner.nextLine();
+                	Cliente cliente = new ClienteRegular(nome, cpf, senha);
+                	
+                	cliente.adicionarConta(investimento);
+                	System.out.println("Usuario adicionado com exito!");
                     break;
-                case "e":
-                    ContaPoupanca contaPoupanca = new ContaPoupanca();
-                    contaPoupanca.executar();
+                case "6":
+                	System.out.println("---- SISTEMA DE CRIAÇÃO DE CONTA GOLD ----");
+                	System.out.println("---- Qual nome do usuario ----");
+                	nome = scanner.nextLine();
+                	System.out.println("---- Qual o CPF do usuario----");
+                	cpf = scanner.nextLine();
+                	System.out.println("---- Qual a Senha do usuario----");
+                	senha = scanner.nextLine();
+                	Cliente clienteGold = new ClienteGold(nome, cpf, senha);
+                	
+                	clienteGold.adicionarConta(investimento);
+                	System.out.println("Usuario adicionado com exito!");
+                	break;
+                case "7":
+                	System.out.println("---- SISTEMA DE CRIAÇÃO DE CONTA VIP ----");
+                	System.out.println("---- Qual nome do usuario ----");
+                	nome = scanner.nextLine();
+                	System.out.println("---- Qual o CPF do usuario----");
+                	cpf = scanner.nextLine();
+                	System.out.println("---- Qual a Senha do usuario----");
+                	senha = scanner.nextLine();
+                	Cliente clienteVIP = new ClienteVip(nome, cpf, senha);
+                	
+                	clienteVIP.adicionarConta(contaCorrente);
+                	System.out.println("Usuario adicionado com exito!");
+                	break;
+                case "8":
+                	System.out.println("Calculando investimento...");
+                	investimento.calcularRendimentoDiario();
                     break;
-                case "f":
-                    ContaCorrente contaCorrente = new ContaCorrente();
-                    contaCorrente.executar();
+                case "9":
+                	contaCorrente.saqueEspecial();
                     break;
-                case "g":
-                    Conta conta = new Conta();
-                    conta.executar();
+                case "10":
+                	poupanca.rendimento();
                     break;
-                case "h":
-                    ClienteVIP clienteVIP = new ClienteVIP();
-                    clienteVIP.executar();
+                case "11":
+                	rendaFixa.calcularRendimento();
                     break;
-                case "i":
-                    ClienteRegular clienteRegular = new ClienteRegular();
-                    clienteRegular.executar();
+                case "12":
+                	rendaFixa.saque();
                     break;
-                case "j":
-                    ClienteGold clienteGold = new ClienteGold();
-                    clienteGold.executar();
-                    break;
-                case "k":
-                    Cliente cliente = new Cliente();
-                    cliente.executar();
-                    break;
-                case "x":
+                case "13":
+                	break;
+                    ''
+                    
+                case "14":
                     System.out.println("Saindo...");
                     break;
                 default:
                     System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
             }
-        } while (!opcao.equals("x"));
+        } while (!opcao.equals("14"));
 
         scanner.close();
     }
